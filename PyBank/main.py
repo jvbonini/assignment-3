aa510a85bc5bb12fff1f5e29ddec8503ad01d10d
Bonini <BONINJV@abbvienet.com>
2019-02-25 14:41:26 -0600

    Commit

---
+++ PyBank/main.py
@@ -0,0 +1,48 @@
+ ### PyBank 
+ 
+ #--------------------------------------------------------------------------------------
+ 
+ # [budget_data.csv](PyBank/Resources/budget_data.csv). The dataset is composed of two columns: `Date` and `Profit/Losses`
+
+ #--------------------------------------------------------------------------------------
+
+ # Python script that analyzes the records to calculate each of the following:
+
+  # 1. The total number of months included in the dataset
+
+
+
+  # 2. The net total amount of "Profit/Losses" over the entire period
+
+
+
+  # 3. The average of the changes in "Profit/Losses" over the entire period
+
+
+
+  # 4. The greatest increase in profits (date and amount) over the entire period
+
+
+
+  # 5. The greatest decrease in losses (date and amount) over the entire period
+
+
+
+  # 6. In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+
+
+
+#--------------------------------------------------------------------------------------
+# Template
+
+  #```text
+  #Financial Analysis
+  #----------------------------
+  #Total Months: 86
+  #Total: $38382578
+  #Average  Change: $-2315.12
+  #Greatest Increase in Profits: Feb-2012 ($1926159)
+  #Greatest Decrease in Profits: Sep-2013 ($-2196167)
+  #```
+#--------------------------------------------------------------------------------------
---
+++ PyPoll/main.py
@@ -0,0 +1,51 @@
+### PyPoll
+
+#--------------------------------------------------------------------------------------
+
+# [election_data.csv](PyPoll/Resources/election_data.csv). The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. 
+
+#--------------------------------------------------------------------------------------
+
+# Python script that analyzes the votes and calculates each of the following:
+ 
+  # 1. The total number of votes cast
+
+
+
+  # 2. A complete list of candidates who received votes
+
+
+
+  # 3. The percentage of votes each candidate won
+
+
+
+  # 4. The total number of votes each candidate won
+
+
+
+  # 5. The winner of the election based on popular vote.
+
+
+
+  # 6. In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+
+#--------------------------------------------------------------------------------------
+# Template
+
+  #```text
+  #Election Results
+  #-------------------------
+  #Total Votes: 3521001
+  #-------------------------
+  #Khan: 63.000% (2218231)
+  #Correy: 20.000% (704200)
+  #Li: 14.000% (492940)
+  #O'Tooley: 3.000% (105630)
+  #-------------------------
+  #Winner: Khan
+  #-------------------------
+  #```
+#--------------------------------------------------------------------------------------
+
